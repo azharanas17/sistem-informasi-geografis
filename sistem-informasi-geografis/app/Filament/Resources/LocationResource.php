51ca7c477bc58c2c2bd5ab1400f9891cbc48e64d
azharanas17 <azharanas173@gmail.com>
2025-08-02 13:35:39 +0700

    add maps data management for admin with filament

---
+++ sistem-informasi-geografis/app/Filament/Resources/LocationResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LocationResource\Pages;
+use App\Filament\Resources\LocationResource\RelationManagers;
+use App\Models\Location;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LocationResource extends Resource
+{
+    protected static ?string $model = Location::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->label('Nama Lokasi')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('info')
+                    ->label('Info Lokasi')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('lat')
+                    ->label('Latitude')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('lng')
+                    ->label('Longitude')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Nama Lokasi')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('info')
+                    ->label('Info Lokasi')
+                    ->toggleable(),
+                Tables\Columns\TextColumn::make('lat')
+                    ->label('Latitude')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('lng')
+                    ->label('Longitude')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLocations::route('/'),
+            'create' => Pages\CreateLocation::route('/create'),
+            'edit' => Pages\EditLocation::route('/{record}/edit'),
+        ];
+    }
+}
